6cdde7880174158cc9fd25decdeedf72d4f6543a
Emily N <eng50@stuy.edu>
2024-09-12 06:38:55 -0400

    Warm Up 1 of Python Coding Bat

---
+++ 02_cb0/parrot_trouble.py
@@ -0,0 +1,5 @@
+def parrot_trouble(talking, hour):
+  if (hour < 7 or hour > 20):
+	return talking
+  return False
+
---
+++ 02_cb0/sleep_in.py
@@ -0,0 +1,6 @@
+def sleep_in(weekday, vacation):
+  if (weekday == False ):
+	return True
+  else:
+	return vacation
+ 
