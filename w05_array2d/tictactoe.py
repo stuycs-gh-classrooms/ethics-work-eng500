190173e2ab06f331d4ae56048be412f83a79eb07
Emily N <eng50@stuy.edu>
2024-09-22 18:39:30 -0400

    fixed it and finished

--- w05_array2d/tictactoe.py
+++ w05_array2d/tictactoe.py
@@ -21,102 +21,82 @@ def arrprint(b):
 #the start() function has no parameters and will run at the
 #beginning of the game to initialize which symbol a player has
 def run():
+    global win
     win = False
+    global b
+    b = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
     print("Player 1 is X and Player 2 is O")
     print("This is a key for the different squares of the board:")
     arrprint(sample)
     while win == False:
         move(p1)
-        win(p1)
+        if win == True:
+            print(f'{p1} wins!')
+            return
+        if tie():
+            print("It's a tie!")
+            return
         move(p2)
-        win(p2)
+        if win == True:
+            print(f'{p2} wins!')
+            return
+        if tie():
+            print("It's a tie!")
+            return
     
 def move(p):
-    print('This is your current board: ')
-    arrprint(b)
     if p == p1:
         pos = input ("Player 1: Which square would you like to move? Please use lowercase letters \n")
     else:
         pos = input ("Player 2: Which square would you like to move? Please use lowercase letters \n")
-    if pos == 'a':
-        if b[0][0] == ' ':
-            b[0][0] = p
+    positions = {'a': (0, 0), 'b': (0, 1), 'c': (0, 2), 'd': (1, 0), 'e': (1, 1), 'f': (1, 2), 'g': (2, 0), 'h': (2, 1), 'i': (2, 2)}
+    
+    if pos in positions:
+        row, col = positions[pos]
+        if b[row][col] == ' ':
+            b[row][col] = p
+            print('This is your current board: ')
+            arrprint(b)
+            c_win(p)
         else:
-           print("This square is taken up. Choose a different one.")
-           move(p)
-    elif pos == 'b':
-        if b[0][1] == ' ':
-            b[0][1] = p
-        else:
-           print("This square is taken up. Choose a different one.")
-           move(p)
-    elif pos == 'c':
-        if b[0][2] == ' ':
-            b[0][2] = p
-        else:
-           print("This square is taken up. Choose a different one.")
-           move(p)
-    elif pos == 'd':
-        if b[1][0] == ' ':
-            b[1][0] = p
-        else:
-           print("This square is taken up. Choose a different one.")
-           move(p)
-    elif pos == 'e':
-        if b[1][1] == ' ':
-            b[1][1] = p
-        else:
-           print("This square is taken up. Choose a different one.")
-           move(p)
-    elif pos == 'f':
-        if b[1][2] == ' ':
-            b[1][2] = p
-        else:
-           print("This square is taken up. Choose a different one.")
-           move(p)
-    elif pos == 'g':
-        if b[2][0] == ' ':
-            b[2][0] = p
-        else:
-           print("This square is taken up. Choose a different one.")
-           move(p)
-    elif pos == 'h':
-        if b[2][1] == ' ':
-            b[2][1] = p
-        else:
-           print("This square is taken up. Choose a different one.")
-           move(p)
-    elif pos == 'i':
-        if b[2][2] == ' ':
-            b[2][2] = p
-        else:
-           print("This square is taken up. Choose a different one.")
-           move(p)
-def win(p):
+            print("This square is taken up. Choose a different one.")
+            move(p)
+    else:
+        print("Please choose a valid square")
+        move(p)
+        
+def c_win(p):
+    global win
     if b[0][0] == p and b[0][1] == p and b[0][2] == p:
         win = True
         print(f"Yay {p} won!")
-    elif b[1][0] == p and b[1][1] == p and b[1][2] == p:
+    if b[1][0] == p and b[1][1] == p and b[1][2] == p:
         win = True
         print(f"Yay {p} won!")
-    elif b[2][0] == p and b[2][1] == p and b[2][2] == p:
+    if b[2][0] == p and b[2][1] == p and b[2][2] == p:
         win = True
         print(f"Yay {p} won!")
-    elif b[0][0] == p and b[1][0] == p and b[2][0] == p:
+    if b[0][0] == p and b[1][0] == p and b[2][0] == p:
         win = True
         print(f"Yay {p} won!")
-    elif b[0][1] == p and b[1][1] == p and b[2][1] == p:
+    if b[0][1] == p and b[1][1] == p and b[2][1] == p:
         win = True
         print(f"Yay {p} won!")
-    elif b[0][2] == p and b[1][2] == p and b[2][2] == p:
+    if b[0][2] == p and b[1][2] == p and b[2][2] == p:
         win = True
         print(f"Yay {p} won!")
-    elif b[0][0] == p and b[1][1] == p and b[2][2] == p:
+    if b[0][0] == p and b[1][1] == p and b[2][2] == p:
         win = True
         print(f"Yay {p} won!")
-    elif b[2][0] == p and b[1][1] == p and b[0][2] == p:
+    if b[2][0] == p and b[1][1] == p and b[0][2] == p:
         win = True
         print(f"Yay {p} won!")
+        
+def tie():
+    for i in b:
+        if ' ' in i:
+            return False
+    return True
             
 run()
 
